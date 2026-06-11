57dc858fcf354aee56d4e6755c41bd3bf638d47a
ziheng <3120372227@qq.com>
2026-06-11 21:07:05 +0800

    项目基本框架

---
+++ terra_admin/src/main/java/com/ziheng/admin/TerraApplication.java
@@ -0,0 +1,11 @@
+package com.ziheng.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TerraApplication.class, args);
+    }
+}
